ecb5459049a91979b2a825e2728fefe8aec839c3
Davide Depau <davide@depau.eu>
2023-10-13 21:05:57 +0200

    Allow running locally

--- SAPISpeechServer/Program.cs
+++ SAPISpeechServer/Program.cs
@@ -10,16 +10,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
