723d13b4d3ea83b4c9e281bcda1befa9f0913187
Davide Depau <davide@depau.eu>
2023-10-14 20:38:53 +0200

    Allow specifying ARM arch version as Docker argument

--- scripts/get_fex_pkgname.py
+++ scripts/get_fex_pkgname.py
@@ -1,43 +1,6 @@
 #!/usr/bin/env python3
 
-# Adapted from https://github.com/FEX-Emu/FEX/blob/main/Scripts/InstallFEX.py#L97
-
-def ListContainsRequired(Features, RequiredFeatures):
-    for Req in RequiredFeatures:
-        if not Req in Features:
-            return False
-    return True
-
-def GetCPUFeaturesVersion():
-    # Also LOR but kernel doesn't expose this
-    v8_1Mandatory = ["atomics", "asimdrdm", "crc32"]
-    v8_2Mandatory = v8_1Mandatory + ["dcpop"]
-    v8_3Mandatory = v8_2Mandatory + ["fcma", "jscvt", "lrcpc", "paca", "pacg"]
-    v8_4Mandatory = v8_3Mandatory + ["asimddp", "flagm", "ilrcpc", "uscat"]
-
-    #  fphp asimdhp asimddp
-
-    File = open("/proc/cpuinfo", "r")
-    Lines = File.readlines()
-    File.close()
-
-    # Minimum spec is ARMv8.0
-    _ArchVersion = "8.0"
-    for Line in Lines:
-        if "Features" in Line:
-            Features = Line.split(":")[1].strip().split(" ")
-
-            # We don't care beyond 8.4 right now
-            if ListContainsRequired(Features, v8_4Mandatory):
-                _ArchVersion = "8.4"
-            elif ListContainsRequired(Features, v8_3Mandatory):
-                _ArchVersion = "8.3"
-            elif ListContainsRequired(Features, v8_2Mandatory):
-                _ArchVersion = "8.2"
-            elif ListContainsRequired(Features, v8_1Mandatory):
-                _ArchVersion = "8.1"
-            break;
-    return _ArchVersion
+import sys
 
 ARMVersionToPackage = {
     "8.0": "fex-emu-armv8.0",
@@ -48,6 +11,9 @@ ARMVersionToPackage = {
 }
 
 if __name__ == "__main__":
-	print(ARMVersionToPackage[GetCPUFeaturesVersion()])
-
-
+    arm_version = sys.argv[1]
+    if arm_version not in ARMVersionToPackage:
+        print("Unsupported ARM version: {}".format(arm_version))
+        print("Supported versions: {}".format(", ".join(ARMVersionToPackage.keys())))
+        sys.exit(1)
+    print(ARMVersionToPackage[arm_version])
